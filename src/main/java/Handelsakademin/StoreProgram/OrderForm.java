432f71fe58724f11c2d4a68ba7f5dfb493589e0e
humlans <spangbergwilma@gmail.com>
2023-11-21 16:17:24 +0100

    Begun orderForm Layout

--- src/main/java/Handelsakademin/StoreProgram/OrderForm.java
+++ src/main/java/Handelsakademin/StoreProgram/OrderForm.java
@@ -1,4 +1,10 @@
 package Handelsakademin.StoreProgram;
 
+import javax.swing.*;
+
 public class OrderForm {
+    private JPanel orderPanel;
+    private JLabel customerNameLabel;
+    private JList productCartJList;
+    private JButton addToCart;
 }
